45d5d9ff9f23ad63bae5f2ad9d25bc53b684beeb
Franco13542 <ffontanarossa@correo.um.edu.uy>
2024-11-20 14:04:22 -0300

    Agrego ventana de Relaxing Videos

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import MotivationalQuote from "./components/MotivationalQuote";
 import CheckInForm from "./components/CheckinForm";
+import RelaxingVideosScreen from "./components/RelaxingVideosScreen";
 import "./styles.css";
 
 const App = () => {
   return (
-    <div className="app-container">
-      <h1 className="app-title">Mental Health Check-In</h1>
-      <MotivationalQuote />
-      <CheckInForm />
-      <div className="button-container">
-        <button className="nav-button">Relaxing Videos</button>
-        <button className="nav-button">Meditation Videos</button>
-        <button className="nav-button">Workout Videos</button>
+    <Router>
+      <div className="app-container">
+        <h1 className="app-title">Mental Health Check-In</h1>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <MotivationalQuote />
+                <CheckInForm />
+                <div className="button-container">
+                  <Link to="/relaxing-videos" className="nav-button">
+                    Relaxing Videos
+                  </Link>
+                  <button className="nav-button">Meditation Videos</button>
+                  <button className="nav-button">Workout Videos</button>
+                </div>
+              </div>
+            }
+          />
+          <Route path="/relaxing-videos" element={<RelaxingVideosScreen />} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 };
 
